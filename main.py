43185b20abac5937de13ebfd36e6667d862c1ccd
GuyWithAQuestn <85851534+GuyWithAQuestn@users.noreply.github.com>
2023-05-20 16:26:58 -0400

    2

--- main.py
+++ main.py
@@ -4,13 +4,35 @@ from selenium.webdriver.chrome.service import Service
 #from webdriver_manager.chrome import ChromeDriverManager
 import undetected_chromedriver as uc
 from bs4 import BeautifulSoup
+import io
+import os
+
+def is_raspberrypi():
+    try:
+        with io.open('/sys/firmware/devicetree/base/model', 'r') as m:
+            if 'raspberry pi' in m.read().lower():
+                return True
+    except Exception:
+        pass
+        return False
 
 
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 #driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-driver = uc.Chrome()
+
+
+# Detect if we're running on a Pi or something else
+if (is_raspberrypi()):
+    print("returned True. This is a Pi")
+    # use the existing chromedriver instead of downloading
+    driver = uc.Chrome(driver_executable_path="/home/pi/.local/share/undetected_chromedriver/chromedriver_copy")
+else:
+    print("retruned False. This is not a Pi.")
+    driver = uc.Chrome()
+
+
 
 driver.get("https://www.nowsecure.nl")
 # search = driver.find_element(by=By.NAME,value="q")
